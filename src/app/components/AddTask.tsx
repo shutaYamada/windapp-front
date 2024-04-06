e555ee751beb14e5a9efb7588cd748b2f3f2e9ce
“HOGE（ShutaYamada）” <hoge@hoge.jpyamadashuta10083150@gmail.com>
2024-04-07 00:31:37 +0900

    Add Taskこんぽーねんと追加

--- src/app/components/AddTask.tsx
+++ src/app/components/AddTask.tsx
@@ -2,9 +2,12 @@ import React from 'react'
 
 const AddTask = () => {
   return (
-    <div>
-      <h1>AddTask</h1>
-    </div>
+    <form className='mb-4 space-y-3' action="">
+        <input className='w-full border px-4 py-2 rounded-lg focus:outline-none focus:border-blue-400' type="text" />
+        <button className='w-full px-4 py-2 text-white bg-blue-500 transform hover:bg-blue-400 hover:scale-95 duration-200'>
+            Add Task
+        </button>
+    </form>
   )
 }
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,8 @@ export default function Home() {
   return (
     <div className='flex flex-col items-center  justify-center min-h-screen py-2 bg-gray-200'>
       <h1 className='text-4xl font-bold text-gray-700'>todoApp</h1>
-      <div>
-        <div>
+      <div className='w-full max-w-xl mt-5'>
+        <div className='w-full px-8 py-6 bg-white shadow-md rounded-lg'>
           <AddTask />
           <TodoList />
         </div>
