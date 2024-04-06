05d2d74da78fbb2566b7a865200d80d948e6a8fb
“HOGE（ShutaYamada）” <hoge@hoge.jpyamadashuta10083150@gmail.com>
2024-04-07 01:07:31 +0900

    id付与

--- src/app/components/TodoList.tsx
+++ src/app/components/TodoList.tsx
@@ -8,7 +8,7 @@ const TodoList = ({ todos }: TodoListProps) => {
   return (
     <ul className='space-y-3'>
         {todos.map((todo) => (
-            <li className='flex justify-between p-4 bg-white border-l-4 border-blue-500 rounded shadow'  >
+            <li key={todo.id} className='flex justify-between p-4 bg-white border-l-4 border-blue-500 rounded shadow'  >
             <span>{todo.title}</span>
             <div >
                 <button className='text-green-500 mr-3'>Edit</button>
